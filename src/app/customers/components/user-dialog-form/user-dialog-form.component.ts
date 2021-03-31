9167a7fb350a2f1f701b49c62ca40516c86e0655
dagboujhatem <dagboujhatem@gmail.com>
2021-03-31 20:58:53 +0100

    create crud user with the same dialog (in create and update user)

--- src/app/customers/components/user-dialog-form/user-dialog-form.component.ts
+++ src/app/customers/components/user-dialog-form/user-dialog-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-dialog-form',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDialogFormComponent implements OnInit {
 
-  constructor() { }
+  userForm : FormGroup;
+  dialogTitle: string;
+  constructor(private dialogRef: MatDialogRef<UserDialogFormComponent>, @Inject(MAT_DIALOG_DATA) private data) {
+    this.userForm = new FormGroup({
+      id: new FormControl(''),
+      fullName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      birthDate: new FormControl('', [Validators.required, Validators.min(0)]),
+      createdAt: new FormControl(''),
+      updatedAt: new FormControl('')
+    });
+   }
 
   ngOnInit(): void {
+    if(this.data !== null)
+    {
+      this.userForm.patchValue(this.data);
+      this.userForm.get('updatedAt').setValue(new Date());
+      this.dialogTitle = 'Update user';
+    }else{
+      this.userForm.get('createdAt').setValue(new Date());
+      this.userForm.get('updatedAt').setValue(new Date());
+      this.dialogTitle = 'Add new user';
+    }
+  }
+
+  saveUser(){
+    this.dialogRef.close(this.userForm.value);
   }
 
 }
--- src/app/customers/components/user-list/user-list.component.ts
+++ src/app/customers/components/user-list/user-list.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { UserService } from '../../services/user.service';
+import { DeleteConfirmDialogComponent } from '../dialog/delete-confirm-dialog/delete-confirm-dialog.component';
+import { UserDialogFormComponent } from '../user-dialog-form/user-dialog-form.component';
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +15,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'fullName', 'email', 'birthDate', 'Actions'];
+  dataSource : MatTableDataSource<any>;
+  constructor(private userService: UserService,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog) { }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getAllUsers().subscribe((response)=>{
+      this.dataSource = new MatTableDataSource<any>(response);
+      // Assign the paginator *after* dataSource is set
+      this.dataSource.paginator = this.paginator;
+      // Assign the sort *after* dataSource is set
+      this.dataSource.sort = this.sort;
+    }, (error)=>{
+        console.log(error);        
+    });
+  }
+
+  addUser() {
+    const createDialog = this.dialog.open(UserDialogFormComponent, {
+      maxWidth: "400px",
+      data: null
+    });
+
+    createDialog.afterClosed().subscribe(result => {
+      if(result !== null)
+      {
+        this.userService.addUser(result).subscribe((response)=>{
+          this.snackBar.open("User has been added successfully!", "Done", {duration: 2000});
+          this.loadUsers();
+        }, (error)=>{
+            console.log(error);        
+        });
+      }
+    });
+  }
+
+  updateUser(user: any){
+    const updateDialog = this.dialog.open(UserDialogFormComponent, {
+      maxWidth: "400px",
+      data: user
+    });
+
+    updateDialog.afterClosed().subscribe(result => {
+      if(result !== null)
+      {
+        this.userService.updateUserById(user.id, result).subscribe((response)=>{
+          this.snackBar.open("User has been updated successfully!", "Done", {duration: 2000});
+          this.loadUsers();
+        }, (error)=>{
+            console.log(error);        
+        });
+      }
+    });
+  }
+
+  deleteUser(userID) {
+    const deleteDialog = this.dialog.open(DeleteConfirmDialogComponent, {
+      maxWidth: "400px",
+      data: {}
+    });
+
+    deleteDialog.afterClosed().subscribe(result => {
+      if(result)
+      {
+        this.userService.deleteUserById(userID).subscribe((response)=>{
+          this.snackBar.open("User has been deleted successfully!", "Done", {duration: 2000});
+          this.loadUsers();
+        }, (error)=>{
+            console.log(error);        
+        });
+      }
+    });
   }
 
 }
